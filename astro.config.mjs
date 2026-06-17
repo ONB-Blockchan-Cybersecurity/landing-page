de8e0541bc81d29686e891fd9f333ad1730e1e03
BloodyCoder <bloody.c0der@gmail.com>
2026-06-17 19:46:12 -0300

    ci: se corrige yml para poder deployar web, se corrige astro.config para levantar en local el proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://ONB-Blockchan-Cybersecurity.github.io",
-  base: "/landing-page",
+  base: process.env.GITHUB_ACTIONS ? "/landing-page" : "/",
 });
